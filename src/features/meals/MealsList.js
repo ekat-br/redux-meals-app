828307b4ab58bfb1a48fa02230a762eecd1491fd
Ekaterina Bruch <ekaterinabruch@gmail.com>
2023-08-30 15:41:46 +0200

    Add functionality for showing more meals

--- src/features/meals/MealsList.js
+++ src/features/meals/MealsList.js
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import MealsCard from "./MealsCard";
 import { useGetAllMealsQuery } from "./mealsApi"
 import { styled } from "styled-components";
 
 export default function MealsList () {
+    const [visibleMeals, setVisibleMeals] = useState(3);
     const {data, isError, isLoading} = useGetAllMealsQuery(); 
     
     if (isLoading){
@@ -13,14 +15,20 @@ export default function MealsList () {
         return <div>Error loading meals.</div>;
     }
 
+    const showMoreMeals = () => {
+        setVisibleMeals(visibleMeals + 2)
+
+    }
+
     return (
         <Container>
             <div>Meals App</div>
             <MealsUList>
-              {data.meals.map((meal) => 
+              {data.meals.slice(0, visibleMeals).map((meal) => 
                <MealsCard key={meal.idMeal} meal={meal}/>
               
               )}
+              <Button onClick={showMoreMeals} disabled={visibleMeals >= data.meals.length}>Show more</Button>
             </MealsUList>
            
         </Container>
@@ -36,6 +44,17 @@ color: #DB5375;
 `
 const MealsUList = styled.ul`
 padding: 10px 40px 10px 40px;
+`
 
+const Button = styled.button`
+color: #ffff; 
+background-color: #ff99ac; 
+padding: 8px 16px; 
+border: none;
+border-radius: 4px; 
+
+&:hover {
+    background-color: #ff5c8a;
+}
 
 `
